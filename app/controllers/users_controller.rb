07e316ef61df8597b057309289e77d277fe92b52
keiji-take <social.200512@gmial.com>
2020-10-26 16:02:34 +0900

    バリデーションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+
   def edit
   end
-
   def update
     if current_user.update(user_params)
       redirect_to root_path
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-
 
   private
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  # presence: true, は空データを登録できなようにしている
+  validates :email, uniqueness: true, presence: true
+  # uniqueness: true, は同じデータを登録できないようにしている
+
 end
