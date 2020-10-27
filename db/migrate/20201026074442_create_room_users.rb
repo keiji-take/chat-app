8380e26d1f2a52b15ae3c3b9f01ca49ad7622257
keiji-take <social.200512@gmial.com>
2020-10-27 15:19:48 +0900

    messagesテーブルの作成

--- db/migrate/20201026074442_create_room_users.rb
+++ db/migrate/20201026074442_create_room_users.rb
@@ -1,6 +1,7 @@
 class CreateRoomUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :room_users do |t|
+                  # foreign_keyで外部キーの指定
       t.references :room, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
